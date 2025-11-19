8906dd8e48f2177af3fd732b85159dbf6120ab55
TruptiDoke008 <truptidoke008@gmail.com>
2025-11-19 16:56:34 +0530

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,7 +3,10 @@ import { test, expect } from '@playwright/test';
 //test are run in serial mode if you want to run parallel mode need to give below command.
 test.describe.configure({mode:'parallel'}); //now test will run in parallel mode.
 
-test('has title', async ({ page }) => {
+test('@Web has title', async ({ page }) => {
+  //@web or /@API or customize annotations use when you only want to run those tests.
+  //npx playwright test ./tests/Basics --grep '@web'     use this to run perticular annotation.
+  
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
